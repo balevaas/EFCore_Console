3cf44fd78c5f5e9e8c3799739fda78a5a4f64585
balevaas <72543214+balevaas@users.noreply.github.com>
2023-05-02 22:42:09 +0700

    Апробация

---
+++ ApplicationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore_Console
+{
+    /// <summary>
+    /// Производный класс от DbContext, определяет контекст, используемый для взаимодействия с бд
+    /// </summary>
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!; // хранит набор объектов, осуществляет связь с таблицей User
+        public ApplicationContext() => Database.EnsureCreated(); // проверка на наличие созданной бд
+
+        /// <summary>
+        /// Подключение к бд
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = efcore.db");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCore_Console;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User tom = new User { Name = "Tom", Age = 15};
+    User alice = new User { Name = "Alice", Age = 15};
+
+    db.Users.Add(alice);
+    db.Users.Add(tom);
+    db.SaveChanges();
+    Console.WriteLine("OK");
+
+    var users = db.Users.ToList();
+    Console.WriteLine("Список людей:");
+    foreach(User u in users)
+    {
+        Console.WriteLine($"{u.ID}.{u.Name} - {u.Age}");
+    }
+}
